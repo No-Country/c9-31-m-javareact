e0e682e875af80e0f23d279baf7f915d75c5fb8a
Emanuel Manrique <manriqueemanuel@gmail.com>
2023-02-21 17:55:43 -0300

    index carrousel

--- front-end/src/components/more-views/index.jsx
+++ front-end/src/components/more-views/index.jsx
@@ -3,36 +3,89 @@ import "./more-views.css";
 import { useNavigate } from "react-router-dom";
 import { ProductCard } from "../product-card";
 import { useProducts } from "../../hooks";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 const MoreViews = (props) => {
   const navigate = useNavigate()
   const data = useProducts()
   console.log(data);
+  const [currentSlide, setCurrentSlide] = React.useState(0);
+  const maxSlides = 4;
+
+  const handlePrevSlide = () => {
+    setCurrentSlide((currentSlide) => Math.max(currentSlide - 1, 0));
+  };
+
+  const handleNextSlide = () => {
+    setCurrentSlide((currentSlide) => Math.min(currentSlide + 1, maxSlides));
+  };
+
   return (
     <>
-  <div style={{maxWidth:"1250px", margin:"30px auto"}}>
-    <h1 className="section-title">{props.title}</h1>
-    <div className="product-carousel-wrapper">
-      <div className="product-carousel-container">
-        <div style={{  display: "flex",
-                      justifyContent: "space-around",
-                      alignItems: "center"}}  >
-          {data.map((p)=>{
-            return  <ProductCard
-            onClick={()=>{navigate("/item/"+p.id , { replace: true })}}
-            key={p.id}
-            sellerName={p.email} 
-            productFoto={p.fotos[0]} 
-            productName={ p.titulo} 
-            descripcion={ p.descripcion} 
-            precio ={p.precioDeVenta} />
-          })}
+      <div style={{ maxWidth: "1250px", margin: "30px auto" }}>
+        <h1 className="section-title">{props.title}</h1>
+        <div className="product-carousel-wrapper">
+          <div className="product-carousel-container">
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <button
+                onClick={handlePrevSlide}
+                disabled={currentSlide === 0}
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  width: "50px",
+                  height: "50px",
+                  borderRadius: "50%",
+                  backgroundColor: "#fff",
+                  border: "2px solid #ddd",
+                }}
+              >
+                <FaChevronLeft size={24} color="#444" />
+              </button>
+              {data
+                .slice(currentSlide, currentSlide + 4)
+                .map((p) => (
+                  <ProductCard
+                    onClick={() => {
+                      navigate("/item/" + p.id, { replace: true });
+                    }}
+                    key={p.id}
+                    sellerName={p.email}
+                    productFoto={p.fotos[0]}
+                    productName={p.titulo}
+                    descripcion={p.descripcion}
+                    precio={p.precioDeVenta}
+                  />
+                ))}
+              <button
+                onClick={handleNextSlide}
+                disabled={currentSlide === maxSlides}
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  width: "50px",
+                  height: "50px",
+                  borderRadius: "50%",
+                  backgroundColor: "#fff",
+                  border: "2px solid #ddd",
+                }}
+                className="right-button-product"
+              >
+                <FaChevronRight size={24} color="#444" />
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-  
-</>
+    </>
   );
 };
 
