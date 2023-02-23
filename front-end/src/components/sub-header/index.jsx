1b58547f53df29b20304011ad82dc7bb69635ce0
Emanuel Manrique <manriqueemanuel@gmail.com>
2023-02-23 12:58:50 -0300

    white screen fixed

--- front-end/src/components/sub-header/index.jsx
+++ front-end/src/components/sub-header/index.jsx
@@ -1,133 +1,150 @@
 import React from "react";
 import { BottomlessButton, CategoryButton } from "../../ui/buttons";
 import { LoginImg, ShoppingBag } from "../../img";
-import "./subHeader.css"
+import "./subHeader.css";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+
 import { Modal, Button } from "react-bootstrap";
 import { useCart, productincart } from "../../hooks";
 import { getProductInCart } from "../../hooks";
 
 export function SubHeader() {
-    // ? Definición de las variables de estado y valores por defecto
-const cart = items;
-const [isOpen, setIsOpen] = useState(false);
-const [position, setPosition] = useState({ x: 0, y: 0 });
-const storedUser = localStorage.getItem("user");
-const [show, setShow] = useState(false);
+  const cart = items;
 
-// ? Definición de las funciones de estado para el modal
-const handleClose = () => setShow(false);
-const handleShow = () => setShow(true);
+  // ? Hooks para controlar si está abierto el submenú
+  const [isOpen, setIsOpen] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
-// ? Definición de la función de navegación
-const navigate = useNavigate();
+  // ? Trae el usuario almacenado en el localstorage
+  const storedUser = localStorage.getItem("user");
 
-// ? Obtención de los items del carrito
-let { items, addItem, removeItem, updateItemQuantity } = useCart();
+  // ? Valida si el modal del carrito debe ser mostrado
+  const [show, setShow] = useState(false);
 
-// ? Definición del componente del Modal del Carrito de Compras
-function CartModal({ show, handleClose }) {
-    const cart = items;
+  // ? Funciones para cerrar o abrir el modal del carrito de compras
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const navigate = useNavigate();
+
+  // ! Revisar esto, funciona con problemas
+  let { items, addItem, removeItem, updateItemQuantity } = useCart(); //
+
+  // ! Revisar esto
+  function CartModal({ show, handleClose }) {
+    const cart = items; // ! Revisar esto
     const [items, setItems] = useState(cart);
 
-    // ? Actualización de los items del carrito al cambiar de página o al refrescar
     useEffect(() => {
-        setItems(cart);
+      setItems(cart);
     }, [cart]);
+  }
 
-}
-
-// ? Función para cerrar sesión
-function logout() {
+  // ? Se activa al presionar salir, elimina el localstorage da como falso el islogged (que sirve para desplegar el submenu de usuario) y finalmente recarga la página para que se apliquen los cambios
+  function logout() {
     localStorage.clear();
     isLogged = false;
     window.location.reload();
-}
+  }
 
-// ? Verificación de si el usuario está logueado o no
-let isLogged = false;
-let email;
-if (storedUser) {
+  // ? En caso de que el usuario esté logeado se hace un json.parse para guardar el mail que se muestra y finalmente se establece a isLogged como true
+  let isLogged = false;
+  let email;
+  if (storedUser) {
     const parsedUser = JSON.parse(storedUser);
     email = parsedUser.email;
     isLogged = true;
-}
+  }
 
-// ? Obtención de los productos en el carrito
-items = getProductInCart();
+  // ? Trae los productos almacenados en items para mostrarlos en el carrito
+  items = getProductInCart();
 
-// ? Manejo del evento click para el botón de inicio de sesión/registro
-function handleClick(event) {
+  // ? Controla que debe hacerse al clickear en el menú de login/usuario según la variable isLogged
+  function handleClick(event) {
     if (isLogged) {
-        setIsOpen(!isOpen);
-        setPosition({ x: event.clientX, y: event.clientY });
-    } else navigate("/login", { replace: true })
-}
+      setIsOpen(!isOpen);
+      setPosition({ x: event.clientX, y: event.clientY });
+    } else navigate("/login", { replace: true });
+  }
 
-// ? Renderizado de los componentes
-return (
+  // ? Retorno del componente
+  return (
     <>
-        <div className="sub_header-conteiner">
-            <div className="category-conteiner">
-                <CategoryButton text="Mujer" arrow={true} />
-                <CategoryButton text="Hombres" arrow={true} />
-                <CategoryButton text="Niños" arrow={true} />
-                <CategoryButton text="Ofertas" arrow={true} />
-                <CategoryButton text="Sobre Nosotros" arrow={true} />
-            </div>
-
-            <div className="category-conteiner">
-                <BottomlessButton onClick={handleClick} text={email ? email : "Iniciar sesión / Registrarse"} img={<LoginImg />}></BottomlessButton>
-                {isOpen && (
-                    <div className="submenu">
-                        <ul className="options-submenu-container">
-                            <li className="options-submenu">Nivel 0</li>
-                            <li className="options-submenu" onClick={() => { navigate("/misventas", { replace: true }) }}>Ventas</li>
-                            <li className="options-submenu">Compras</li>
-                            <li className="options-submenu">Configuracion</li>
-                            <li className="options-submenu">Perfil</li>
-                            <li className="options-submenu" onClick={() => logout()}>Salir</li>
-                        </ul>
-                    </div>
-                )}
-                <BottomlessButton text="Carrito" img={<ShoppingBag />} onClick={handleShow}></BottomlessButton>
-            </div>
+      <div className="sub_header-conteiner">
+        <div className="category-conteiner">
+          <CategoryButton text="Mujer" arrow={true} />
+          <CategoryButton text="Hombres" arrow={true} />
+          <CategoryButton text="Niños" arrow={true} />
+          <CategoryButton text="Ofertas" arrow={true} />
+          <CategoryButton text="Sobre Nosotros" arrow={true} />
         </div>
 
-        <Modal show={show} onHide={handleClose} animation={true}>
-            <Modal.Header closeButton>
-                <Modal.Title>Carrito de compras</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                {/* Faltan los estilos  */}
-                {productincart ? (
-                    productincart.map((item) => (
-                        <div key={item.id}>
-                            <p>{item.titulo}</p>
-                            <p>${item.precioDeVenta}</p>
-                            <button onClick={() => removeItem(item.id)}>Remover</button>
-                        </div>
-                    ))
-                ) : (
-                    <p>El carrito está vacío.</p>
-                )
-
-                }
-            </Modal.Body>
-
-            <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                    Cerrar
-                </Button>
-                <Button
-                    variant="primary"
-                    onClick={() => navigate("/checkout", { replace: true })}
+        <div className="category-conteiner">
+          <BottomlessButton
+            onClick={handleClick}
+            text={email ? email : "Iniciar sesión / Registrarse"}
+            img={<LoginImg />}
+          ></BottomlessButton>
+          {isOpen && (
+            <div className="submenu">
+              <ul className="options-submenu-container">
+                <li className="options-submenu">Nivel 0</li>
+                <li
+                  className="options-submenu"
+                  onClick={() => {
+                    navigate("/misventas", { replace: true });
+                  }}
                 >
-                    Ir a la compra
-                </Button>
-            </Modal.Footer>
-        </Modal>
+                  Ventas
+                </li>
+                <li className="options-submenu">Compras</li>
+                <li className="options-submenu">Configuracion</li>
+                <li className="options-submenu">Perfil</li>
+                <li className="options-submenu" onClick={() => logout()}>
+                  Salir
+                </li>
+              </ul>
+            </div>
+          )}
+          <BottomlessButton
+            text="Carrito"
+            img={<ShoppingBag />}
+            onClick={handleShow}
+          ></BottomlessButton>
+        </div>
+      </div>
+      <Modal show={show} onHide={handleClose} animation={true}>
+        <Modal.Header closeButton>
+          <Modal.Title>Carrito de compras</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {/* Faltan los estilos  */}
+          {productincart ? (
+            productincart.map((item) => (
+              <div key={item.id}>
+                <p>{item.titulo}</p>
+                <p>${item.precioDeVenta}</p>
+                <button onClick={() => removeItem(item.id)}>Remover</button>
+              </div>
+            ))
+          ) : (
+            <p>El carrito está vacío.</p>
+          )}
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cerrar
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() => navigate("/checkout", { replace: true })}
+          >
+            Ir a la compra
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
-)
-};
+  );
+}
