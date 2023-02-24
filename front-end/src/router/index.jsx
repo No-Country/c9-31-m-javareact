17f71084e012ebead75a9eabd8ff1065e9e68d9b
Emanuel Manrique <manriqueemanuel@gmail.com>
2023-02-24 19:03:33 -0300

    product view

--- front-end/src/router/index.jsx
+++ front-end/src/router/index.jsx
@@ -28,6 +28,8 @@ function AppRoutes() {
 let user = localStorage.getItem("user");
 if (user != undefined) user = true
 
+console.log("Wenas, tengo que sentirme true para que ande todo. Ahora me siento: " + user)
+
     return (
         <Routes>
             <Route path="/" element={<Layout/>} >
