e84aedb85cd523bd08b5871fca4aefa15592a723
ale-strada <79310707+ale-strada@users.noreply.github.com>
2023-02-28 17:04:25 -0300

    header responsive

--- front-end/src/components/header/index.jsx
+++ front-end/src/components/header/index.jsx
@@ -1,15 +1,34 @@
-import React from "react";
-import { Regalo } from "../../img";
+import React, {useState} from "react";
+import { Regalo,WhiteSearchIcon, WhiteShoppingBagIcon, WhiteUserIcon } from "../../img";
 import { Logo } from "../../ui/logo/index";
 import { SearchBar } from "../search-bar/index";
-import { Link } from "react-router-dom";
 import "./header.css"
 import { useUser } from "../../hooks";
+import Hamburger from 'hamburger-react'
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Header() {
+const navigate = useNavigate()
+const [isOpen, setOpen] = useState(false)
+
+
 useUser()
     return <div className="header-conteiner">
+        <div className="responsive-wrap">
+            <Hamburger color="white" type="titl" toggled={isOpen} toggle={setOpen} />
+        </div>
         <Logo />
+        <div className="responsive-wrap">
+            <WhiteSearchIcon onClick={()=>{navigate("/resultados/", { replace: true })}}/>
+            <WhiteUserIcon onClick={()=>{console.log("ogin");}}/>
+            <WhiteShoppingBagIcon onClick={()=>{console.log("carrito");}}/>
+        </div>
+        {isOpen? <div className="menu-header-responsive">
+            <ul>
+                <li className="menu-header-responsive_option" onClick={()=>{navigate("/sell", { replace: true });setOpen(!isOpen)}}>Vender</li>
+            </ul>
+        </div>:"" }
+       
         <SearchBar />
         <div className="header-buttons">
             <Link to="/sell">
